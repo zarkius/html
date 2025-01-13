db739303a41173e8be1224fccb3976573d912c30
zarkius <diegomallor2.0@gmail.com>
2025-01-13 12:14:12 +0100

    deleted:    js/bootstrap.js 	deleted:    js/custom.js 	deleted:    js/jquery-3.4.1.min.js

--- js/custom.js
+++
@@ -1,7 +0,0 @@
-// Get Current Year
-function getCurrentYear() {
-    var d = new Date();
-    var year = d.getFullYear();
-    document.querySelector("#displayDateYear").innerText = year;
-}
-console.log(getCurrentYear());
